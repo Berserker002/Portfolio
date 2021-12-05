e7d6a7d3f221168bf2aab992b363cc2a0c91ed0d
Abhi <abhi.barbiyal@gmail.com>
2021-12-05 20:03:00 +0530

    product feature added

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import './productList.css'
 import Product from '../Product/Product'
+import {products} from "../../data"
 
 const ProductList = () => {
-    return (
-        <div className="pl">
-            <div className="pl-text">
-            <h1 className="pl-title">
-                Create & Inspire
-            </h1>
-            <div className="pl-desc">
-                This is a place to take a look at beautiful models
-                waiting for you and your pipi.
-            </div> 
-            </div>
-            <div className="pl-list">
-            <Product/>
-            <Product/>
-            <Product/>
-            </div>
+  return (
+    <div className="pl">
+      <div className="pl-text">
+        <h1 className="pl-title">Create & Inspire</h1>
+        <div className="pl-desc">
+          This is a place to take a look at beautiful models waiting for you and
+          your pipi.
         </div>
-    )
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default ProductList
---
+++ src/data.js
@@ -0,0 +1,22 @@
+import p1 from './img/p1.jpg'
+import p2 from './img/p2.jpg'
+import p3 from './img/p3.jpg'
+
+
+export const products = [
+    {
+      id: 1,
+      img: p1,
+      link: "https://www.youtube.com/channel/UCwQFiOn2RmulepDejxNt0lQ/videos",
+    },
+    {
+      id: 2,
+      img: p2,
+      link: "https://www.youtube.com/channel/UCwQFiOn2RmulepDejxNt0lQ/videos",
+    },
+    {
+      id: 3,
+      img: p3,
+      link: "https://www.youtube.com/channel/UCwQFiOn2RmulepDejxNt0lQ/videos",
+    },
+  ];
